77131099fe3c8c1a14fe89473e046a7f3467f86a
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-04-24 16:32:00 -0300

    Update rendimento_atleta.py

--- lista1/rendimento_atleta.py
+++ lista1/rendimento_atleta.py
@@ -1,3 +1,4 @@
+# Esse codigo calcula a velocidade media do piloto em uma prova de corrida, com base da Distancia e no Tempo
 piloto = ['Max Verstappen', 'Sergio Perez']
 piloto = input()
 distanciaTotal = float(input())
