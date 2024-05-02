122b2b6886c78dcd915b5067c64904d35d639000
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-05-02 16:29:21 -0300

    Create NickName.py

---
+++ lista_treinamento/NickName.py
@@ -0,0 +1,11 @@
+# Nick Name
+# A variavel (N e S) é chamada e depois há uma verificação se tem no minimo 3 e no maximo 16 caracteres 
+N = input()
+N [3:16]
+S = input()
+S [3:16]
+
+# Variavel "K" une os 2 nomes escolhidos para formar o Nick do Jogo
+K = N + S
+
+print(K)
