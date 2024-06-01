e7b250660926435778dd59ede80670bb362a4df8
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-06-01 19:26:24 -0300

    Create doacao.py

---
+++ lista_treinamento/doacao.py
@@ -0,0 +1,11 @@
+# quantidade
+quantidade_ferro = int(input())
+
+# calculo de divisao inteira
+pack_receber = quantidade_ferro // 3
+
+# vendo os restos a serem dispensados 
+pack_dispensado = quantidade_ferro % 3
+
+# printa printa printa printa
+print (f"{pack_receber}\n{pack_dispensado}")
